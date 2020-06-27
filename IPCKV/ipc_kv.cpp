b62c1a2c44682f74710867da97ee28f8732f8245
vldr <vlader101@gmail.com>
2020-06-26 19:50:21 -0500

    Changed functionality of hashtable.

--- IPCKV/ipc_kv.cpp
+++ IPCKV/ipc_kv.cpp
@@ -1,6 +1,340 @@
 #include "ipc_kv.h"
 
-IPC_KV::IPC_KV(std::string name) :
+IPC_KV::IPC_KV(const std::string& name)
+{
+	m_name = name;
+
+	initialize_info(m_name);
+	initialize_data(m_name);
+}
+
+IPC_KV::~IPC_KV()
+{
+	if (m_data)
+		UnmapViewOfFile(m_data);
+
+	if (m_info)
+		UnmapViewOfFile(m_info);
+
+	if (m_data_handle)
+		CloseHandle(m_data_handle);
+
+	if (m_info_handle)
+		CloseHandle(m_info_handle);
+}
+
+bool IPC_KV::set(const std::string& key, unsigned char* data, size_t size)
+{
+	if (size >= DATA_SIZE)
+		return false;
+
+	IPC_WriteLock write_lock(m_lock_path);
+
+	if (LOAD_FACTOR >= MAX_LOAD_FACTOR)
+		resize();
+	else
+		check_capacity();
+
+	uint32_t probeIndex = 0;
+	uint32_t bucketsProbed = 0;
+
+	uint32_t hashCode = hash(key.c_str(), key.length());
+	uint32_t bucket = hashCode % m_info->capacity;
+
+	while (bucketsProbed < m_info->capacity)
+	{
+		if (m_data[bucket].state != IPC_KV_Data_State::Occupied || m_data[bucket].key == key)
+		{ 
+			if (m_data[bucket].state != IPC_KV_Data_State::Occupied)
+				m_info->size++;
+
+			m_data[bucket].size = size;
+			m_data[bucket].state = IPC_KV_Data_State::Occupied;
+
+			memcpy(m_data[bucket].data, data, size);
+			strncpy_s(m_data[bucket].key, key.c_str(), key.length());
+
+			return true;
+		}
+
+		printf("Collision! %s -> %d\n", key.c_str(), bucket);
+
+		probeIndex++;
+
+		bucket = (hashCode + C1_CONSTANT * probeIndex + C2_CONSTANT * probeIndex * probeIndex) % m_info->capacity;
+		bucketsProbed++;
+	}
+
+	return false;
+}
+
+void IPC_KV::print()
+{
+	for (size_t i = 0; i < m_info->capacity; i++)
+	{
+		if (m_data[i].state == IPC_KV_Data_State::Occupied)
+			printf("[%d] %s 0x%X\n", i, m_data[i].key, m_data[i].size);
+	}
+
+	printf("Capacity %d, Size %d, Resizes %d, Load Factor %f\n", m_info->capacity, m_info->size, m_info->resize_count, LOAD_FACTOR);
+}
+
+size_t IPC_KV::size()
+{
+	return m_info->size;
+}
+
+size_t IPC_KV::find_nearest_prime(size_t input)
+{
+	while (!is_prime(input))
+	{
+		input++;
+	}
+
+	return input;
+}
+
+bool IPC_KV::is_prime(size_t input) 
+{
+	for (int i = 2; i <= (int)sqrt(input); i++)
+	{
+		if (input % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void IPC_KV::check_capacity()
+{
+	if (m_cached_resize_count != m_info->resize_count)
+	{
+		printf("Capacity has changed, fetching new memory\n");
+
+		if (m_data)
+			UnmapViewOfFile(m_data);
+
+		if (m_data_handle)
+			CloseHandle(m_data_handle);
+
+		initialize_data(m_name);
+
+		m_cached_resize_count = m_info->resize_count;
+		m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
+	}
+}
+
+
+void IPC_KV::resize()
+{
+	printf("Resizing memory\n");
+
+	HANDLE previous_data_handle = m_data_handle;
+	IPC_KV_Data* previous_data = m_data;
+	size_t previous_capacity = m_info->capacity;
+
+	m_info->resize_count++;
+	m_info->size = 0;
+	m_info->capacity = find_nearest_prime(m_info->capacity * 2);
+
+	initialize_data(m_name);
+
+	m_cached_resize_count = m_info->resize_count;
+	m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
+	
+	for (size_t i = 0; i < previous_capacity; i++)
+	{
+		if (previous_data[i].state == IPC_KV_Data_State::Occupied)
+		{
+			set(previous_data[i].key, previous_data[i].data, previous_data[i].size);
+		}
+	}
+
+	if (previous_data)
+		UnmapViewOfFile(previous_data);
+
+	if (previous_data_handle)
+		CloseHandle(previous_data_handle);	
+}
+
+uint32_t IPC_KV::hash(const char * key, int count)
+{
+	typedef uint32_t* P;
+	uint32_t h = 0x811c9dc5;
+	while (count >= 8) {
+		h = (h ^ ((((*(P)key) << 5) | ((*(P)key) >> 27)) ^ *(P)(key + 4))) * 0xad3e7;
+		count -= 8;
+		key += 8;
+	}
+#define tmp h = (h ^ *(uint16_t*)key) * 0xad3e7; key += 2;
+	if (count & 4) { tmp tmp }
+	if (count & 2) { tmp }
+	if (count & 1) { h = (h ^ *key) * 0xad3e7; }
+#undef tmp
+	return h ^ (h >> 16);
+}
+
+void IPC_KV::initialize_data(const std::string& name)
+{
+	auto handle_path = "ipckv_" + std::to_string(m_info->resize_count) + "_" + name;
+
+	if (handle_path.length() > MAX_PATH)
+	{
+		throw std::runtime_error("key is too long.");
+	}
+
+	m_data_handle = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(IPC_KV_Data) * m_info->capacity,
+		handle_path.c_str()
+	);
+
+	if (m_data_handle == NULL)
+	{
+		throw std::runtime_error("could not create file mapping object.");
+	}
+
+	auto does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
+
+	auto buffer = MapViewOfFile(
+		m_data_handle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(IPC_KV_Data) * m_info->capacity
+	);
+
+	if (buffer == NULL)
+	{
+		CloseHandle(m_data_handle);
+
+		throw std::runtime_error("could not map view of file.");
+	}
+
+	m_data = (IPC_KV_Data*)buffer;
+
+	if (!does_already_exist)
+	{
+		printf("Initializing data %s...\n", handle_path.c_str());
+		std::memset(m_data, 0, sizeof(IPC_KV_Data) * m_info->capacity);
+	}
+}
+
+void IPC_KV::initialize_info(const std::string& name)
+{
+	auto handle_path = "ipckv_i_" + name;
+
+	if (handle_path.length() > MAX_PATH)
+	{
+		throw std::runtime_error("key is too long.");
+	}
+
+	m_info_handle = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(IPC_KV_Info),
+		handle_path.c_str()
+	);
+
+	if (m_info_handle == NULL)
+	{
+		throw std::runtime_error("could not create file mapping object.");
+	}
+
+	bool does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
+
+	auto buffer = MapViewOfFile(
+		m_info_handle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(IPC_KV_Info)
+	);
+
+	if (buffer == NULL)
+	{
+		CloseHandle(m_info_handle);
+
+		throw std::runtime_error("could not map view of file.");
+	}
+
+	m_info = (IPC_KV_Info*)buffer;
+
+	if (!does_already_exist)
+	{
+		printf("Initializing info %s...\n", handle_path.c_str());
+
+		m_info->size = 0;
+		m_info->resize_count = 0;
+		m_info->capacity = INITIAL_CAPACITY;
+	}
+
+	m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
+	m_cached_resize_count = m_info->resize_count;
+}
+
+int main()
+{
+	unsigned char lol[] = { 0x68, 0x69 };
+	 
+	auto kv = IPC_KV("test");      
+	    
+	kv.set("a", lol, sizeof(lol));
+	kv.set("b", lol, sizeof(lol));
+	kv.set("c", lol, sizeof(lol));
+	kv.set("d", lol, sizeof(lol));
+	kv.set("e", lol, sizeof(lol));
+	kv.set("f", lol, sizeof(lol));
+	kv.print();
+
+	kv.set("Hel Worldgyuguy", lol, sizeof(lol));
+	kv.print();
+
+
+	printf("Finished execution, %d\n", kv.size());
+
+	/*auto kv = new IPC_KV("test");
+
+	std::thread lol([kv]() {
+		while (true)
+		{
+			kv->set("lol", "hello world2");
+		}
+	});
+
+	std::thread lol3([kv]() {
+		while (true)
+		{
+			if (GetAsyncKeyState(VK_DELETE))
+				kv->clear();
+
+			auto object = kv->get("lol");
+
+			printf("%s\n", object.size() ? (const char*)object.buffer() : "null");
+
+
+
+
+		}
+	});
+
+	lol.detach();
+	lol3.detach();
+	*/
+	Sleep(500000000);
+
+	return 0;
+}
+
+
+/*IPC_KV::IPC_KV(std::string name) :
 	m_name(std::move(name)),
 	m_handle_prefix("Global\\IPCKV_" + m_name + "_"),
 	m_lock_prefix("IPCKV_" + m_name + "_")
@@ -9,29 +343,27 @@ IPC_KV::IPC_KV(std::string name) :
 
 	if (m_event_name.length() > MAX_PATH)
 	{
-		throw std::exception("too long name.");
+		throw std::runtime_error("too long name.");
 	}
 
 	m_clear_event_handle = CreateEvent(NULL, FALSE, FALSE, m_event_name.c_str());
 
 	if (m_clear_event_handle == nullptr)
 	{
-		throw std::exception("unable to create event for clearing. " + GetLastError());
+		throw std::runtime_error("unable to create event for clearing. " + GetLastError());
 	}
 
 	//////////////////////////////////////////////////
 
-	auto clear_callback = [](PVOID p, BOOLEAN b) 
-	{
-		auto object = (IPC_KV*)p;
-
-		object->destroy();
-	};
-
 	auto result = RegisterWaitForSingleObject(
 		&m_clear_wait_handle, 
 		m_clear_event_handle, 
-		clear_callback, 
+		[](PVOID this_pointer, BOOLEAN b)
+		{
+			auto object = (IPC_KV*)this_pointer;
+
+			object->destroy();
+		},
 		this, 
 		INFINITE, 
 		WT_EXECUTEDEFAULT
@@ -39,7 +371,7 @@ IPC_KV::IPC_KV(std::string name) :
 
 	if (!result)
 	{
-		throw std::exception("unable to register for event. " + GetLastError());
+		throw std::runtime_error("unable to register for event. " + GetLastError());
 	}
 }
 
@@ -63,11 +395,8 @@ void IPC_KV::destroy()
 	std::lock_guard<std::mutex> guard(m_mutex);
 	for (auto item : m_buffers)
 	{
-		if (item.second.first->size)
-		{
-			IPC_KV::IPC_WriteLock lock(m_lock_prefix + item.first);
-			item.second.first->size = 0;
-		}
+		IPC_KV::IPC_WriteLock lock(m_lock_prefix + item.first);
+		item.second.first->size = 0;
 	}
 }
 
@@ -78,15 +407,20 @@ void IPC_KV::clear()
 
 IPC_KV::IPC_Value IPC_KV::get(const std::string & key)
 {
+	return IPC_Value(key, m_lock_prefix + key, get_object(key));
+}
+
+IPC_KV::IPC_Object* IPC_KV::get_object(const std::string & key)
+{
 	std::lock_guard<std::mutex> guard(m_mutex);
 	auto buffer = m_buffers.find(key);
 
 	if (buffer == m_buffers.end())
 	{
-		return IPC_Value(key, get_handle(key));
+		return get_handle(key);
 	}
 
-	return IPC_Value(key, buffer->second.first);
+	return buffer->second.first;
 }
 
 void IPC_KV::set(const std::string& key, const std::string& value)
@@ -98,14 +432,19 @@ void IPC_KV::set(const std::string& key, const uint8_t * value, const size_t len
 {
 	if (len > BLOCK_SIZE)
 	{
-		throw std::exception("object too large, increase block size");
+		throw std::runtime_error("object too large, increase block size");
 	}
 
-	auto ipc_object = get(key);
+	auto ipc_object = get_object(key);
 
 	if (ipc_object == nullptr)
 	{
 		auto handle_path = m_handle_prefix + key;
+
+		if (handle_path.length() > MAX_PATH)
+		{
+			throw std::runtime_error("key is too long.");
+		}
 
 		auto map_file_handle = CreateFileMapping(
 			INVALID_HANDLE_VALUE, 
@@ -118,7 +457,7 @@ void IPC_KV::set(const std::string& key, const uint8_t * value, const size_t len
 
 		if (map_file_handle == NULL)
 		{
-			throw std::exception("could not create file mapping object (set)");
+			throw std::runtime_error("could not create file mapping object. (set)");
 		}
 
 		auto buffer = MapViewOfFile(
@@ -133,7 +472,7 @@ void IPC_KV::set(const std::string& key, const uint8_t * value, const size_t len
 		{
 			CloseHandle(map_file_handle);
 
-			throw std::exception("could not map view of file. (set)");
+			throw std::runtime_error("could not map view of file. (set)");
 		}
 
 		////////////////////////////////////////////////
@@ -154,6 +493,11 @@ IPC_KV::IPC_Object* IPC_KV::get_handle(const std::string & key)
 {
 	auto handle_path = m_handle_prefix + key;
 	 
+	if (handle_path.length() > MAX_PATH)
+	{
+		throw std::runtime_error("key is too long.");
+	}
+
 	auto map_file_handle = OpenFileMapping(
 		FILE_MAP_ALL_ACCESS,
 		FALSE,
@@ -165,7 +509,7 @@ IPC_KV::IPC_Object* IPC_KV::get_handle(const std::string & key)
 		if (GetLastError() == ERROR_FILE_NOT_FOUND)
 			return nullptr;
 
-		throw std::exception("could not open file mapping object. (get_handle)");
+		throw std::runtime_error("could not open file mapping object. (get_handle)");
 	}
 
 	/////////////////////////////////////////////////
@@ -180,7 +524,7 @@ IPC_KV::IPC_Object* IPC_KV::get_handle(const std::string & key)
 
 	if (buffer == NULL)
 	{
-		throw std::exception("could not map view of file. (get_handle)");
+		throw std::runtime_error("could not map view of file. (get_handle)");
 	}
 
 	/////////////////////////////////////////////////
@@ -190,32 +534,4 @@ IPC_KV::IPC_Object* IPC_KV::get_handle(const std::string & key)
 
 	return ipc_object;
 }
-
-int main()
-{
-	auto kv = new IPC_KV("test");
-
-	std::thread lol([kv]() {
-		while (true) 
-		{
-			kv->set("lol", "hello world2");
-		} 
-	});
-	
-	std::thread lol3([kv]() {
-		while (true)
-		{
-			if (GetAsyncKeyState(VK_DELETE))
-				kv->clear();
-
-			printf("%s\n", kv->get("lol") && kv->get("lol")->size ? (const char*)kv->get("lol")->buffer : "null");
-		}
-	}); 
-
-	lol.detach();
-	lol3.detach();
-	
-	Sleep(500000000);
-
-	return 0;
-}
+*/
--- IPCKV/ipc_kv.h
+++ IPCKV/ipc_kv.h
@@ -5,172 +5,253 @@
 #include <mutex>
 
 #define LOG(...) printf(__VA_ARGS__)
-#define BLOCK_SIZE 4096
 #define MAX_LOCKS 24
 
-class IPC_KV {
+#define MAX_LOAD_FACTOR 0.6f
+#define INITIAL_CAPACITY 10
+#define DATA_SIZE 2048
+#define KEY_SIZE 260
+
+#define LOAD_FACTOR (float)m_info->size / (float)m_info->capacity
+
+#define C1_CONSTANT 3
+#define C2_CONSTANT 5
+
+class IPC_KV 
+{
 public:
-#define MAX_LOCKS 2
-
-	class IPC_ReadLock {
-	public:
-		IPC_ReadLock(const IPC_ReadLock&) = delete;
-		IPC_ReadLock& operator=(IPC_ReadLock const&) = delete;
-
-		IPC_ReadLock(const std::string& name)
-		{
-			if (name.length() > MAX_PATH)
-			{
-				throw std::runtime_error("rwlock name too long.");
-			}
-
-			semaphore_handle = CreateSemaphoreA(
-				nullptr,
-				MAX_LOCKS,
-				MAX_LOCKS,
-				name.c_str()
-			);
-
-			if (semaphore_handle == nullptr)
-			{
-				throw std::runtime_error("could not create rwlock.");
-			}
-
-			auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
-
-			if (wait_result != WAIT_OBJECT_0)
-				throw std::runtime_error("failed to wait for single object");
-		}
-
-		~IPC_ReadLock() noexcept
-		{
-			if (semaphore_handle)
-			{
-				ReleaseSemaphore(semaphore_handle, 1, nullptr);
-				CloseHandle(semaphore_handle);
-			}
-		}
-
-	private:
-		HANDLE semaphore_handle = nullptr;
-	};
-
-	class IPC_WriteLock {
-	public:
-		IPC_WriteLock(const std::string& name)
-		{
-			if (name.length() > MAX_PATH)
-			{
-				throw std::runtime_error("rwlock name too long.");
-			}
-
-			auto mutex_name = name + "_mutex";
-
-			mutex_handle = CreateMutexA(
-				nullptr,
-				FALSE,
-				mutex_name.c_str()
-			);
-
-			if (mutex_handle == nullptr)
-			{
-				throw std::runtime_error("could not create mutex.");
-			}
-
-			auto wait_result = WaitForSingleObject(mutex_handle, INFINITE);
-
-			if (wait_result != WAIT_OBJECT_0)
-				throw std::runtime_error("failed to wait for semaphore object");
-
-			//////////////////////////////////////////////
-
-			semaphore_handle = CreateSemaphoreA(
-				nullptr,
-				0,
-				MAX_LOCKS,
-				name.c_str()
-			);
-
-			if (semaphore_handle == nullptr)
-			{
-				throw std::runtime_error("could not create rwlock.");
-			}
-
-			if (GetLastError() == ERROR_ALREADY_EXISTS)
-			{
-				for (int i = 0; i < MAX_LOCKS; i++)
-				{
-					auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
-
-					if (wait_result != WAIT_OBJECT_0)
-						throw std::runtime_error("failed to wait for semaphore object");
-				}
-			}
-		}
-
-		~IPC_WriteLock() noexcept
-		{
-			if (semaphore_handle)
-			{
-				ReleaseSemaphore(semaphore_handle, MAX_LOCKS, nullptr);
-				CloseHandle(semaphore_handle);
-			}
-
-			if (mutex_handle)
-			{
-				ReleaseMutex(mutex_handle);
-				CloseHandle(mutex_handle);
-			}
-		}
-
-	private:
-		HANDLE semaphore_handle = nullptr;
-		HANDLE mutex_handle = nullptr;
-	};
-
-	struct IPC_Object 
-	{
-		size_t size = 0;
-		uint8_t buffer[BLOCK_SIZE];
-	};
-
-	class IPC_Value {
-	public:
-		IPC_Value(const std::string & key, IPC_Object* ipc_object) 
-			: m_ipc_object(ipc_object), m_read_lock(key) {}
-	private:
-		IPC_Object* m_ipc_object;
-		IPC_ReadLock m_read_lock;
-	};
-
-	IPC_KV(std::string name);
+	IPC_KV(const std::string& name);
 	~IPC_KV();
 
-	IPC_KV::IPC_Value get(const std::string&);
-	void set(const std::string& key, const std::string& value);
-	void set(const std::string&, const uint8_t*, const size_t);
-
-	void destroy();
-	void clear();
-
-	IPC_KV(const IPC_KV&) = delete;
-	IPC_KV(IPC_KV&&) = delete;
-
-	IPC_KV& operator=(const IPC_KV&) = delete;
-	IPC_KV& operator=(IPC_KV&&) = delete;
-
-	std::string m_name{};
-	std::string m_handle_prefix{};
-	std::string m_lock_prefix{};
-
-	std::mutex m_mutex;
+	bool set(const std::string& key, unsigned char* data, size_t size);
+	void print();
+	size_t size();
 private:
-	IPC_Object* get_handle(const std::string & key);
+	enum IPC_KV_Data_State
+	{
+		Empty = 0,
+		Deleted = 1,
+		Occupied = 2,
+	};
 
-	std::unordered_map<std::string, std::pair<IPC_Object*, void*>> m_buffers{};
+	struct IPC_KV_Info
+	{
+		size_t capacity;
+		size_t size;
 
-	void* m_clear_event_handle;
-	void* m_clear_wait_handle;
+		size_t resize_count;
+	};
 
-	
+	struct IPC_KV_Data
+	{
+		IPC_KV_Data_State state;
+
+		char key[KEY_SIZE];
+
+		unsigned char data[DATA_SIZE];
+		size_t size;
+	};
+
+	size_t find_nearest_prime(size_t input);
+	bool is_prime(size_t input);
+
+	void resize();
+	void check_capacity();
+
+	uint32_t hash(const char* key, int count);
+
+	void initialize_info(const std::string& name);
+	void initialize_data(const std::string& name);
+
+	IPC_KV_Info* m_info = nullptr;
+	IPC_KV_Data* m_data = nullptr;
+
+	HANDLE m_info_handle = nullptr;
+	HANDLE m_data_handle = nullptr;	
+
+	std::string m_name;
+	std::string m_lock_path;
+
+	size_t m_cached_resize_count;
 };
+
+class IPC_ReadLock {
+public:
+	IPC_ReadLock(const std::string& name)
+	{
+		if (name.length() > MAX_PATH)
+		{
+			throw std::runtime_error("rwlock name too long.");
+		}
+
+		semaphore_handle = CreateSemaphoreA(
+			nullptr,
+			MAX_LOCKS,
+			MAX_LOCKS,
+			name.c_str()
+		);
+
+		if (semaphore_handle == nullptr)
+		{
+			throw std::runtime_error("could not create rwlock.");
+		}
+
+		auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
+
+		if (wait_result != WAIT_OBJECT_0)
+			throw std::runtime_error("failed to wait for single object");
+	}
+
+	~IPC_ReadLock() noexcept
+	{
+		if (semaphore_handle)
+		{
+			ReleaseSemaphore(semaphore_handle, 1, nullptr);
+			CloseHandle(semaphore_handle);
+		}
+	}
+
+	IPC_ReadLock(const IPC_ReadLock&) = delete;
+	IPC_ReadLock& operator=(IPC_ReadLock const&) = delete;
+
+	IPC_ReadLock(IPC_ReadLock&& ipc_read_lock) noexcept :
+		semaphore_handle(std::exchange(ipc_read_lock.semaphore_handle, nullptr)) { }
+
+	IPC_ReadLock& operator=(IPC_ReadLock&& ipc_read_lock)
+	{
+		semaphore_handle = std::exchange(ipc_read_lock.semaphore_handle, nullptr);
+
+		return *this;
+	}
+
+private:
+	HANDLE semaphore_handle = nullptr;
+};
+
+class IPC_WriteLock {
+public:
+	IPC_WriteLock(const std::string& name)
+	{
+		if (name.length() > MAX_PATH)
+		{
+			throw std::runtime_error("rwlock name too long.");
+		}
+
+		auto mutex_name = name + "_mutex";
+
+		mutex_handle = CreateMutexA(
+			nullptr,
+			FALSE,
+			mutex_name.c_str()
+		);
+
+		if (mutex_handle == nullptr)
+		{
+			throw std::runtime_error("could not create mutex.");
+		}
+
+		auto wait_result = WaitForSingleObject(mutex_handle, INFINITE);
+
+		if (wait_result != WAIT_OBJECT_0)
+			throw std::runtime_error("failed to wait for semaphore object");
+
+		//////////////////////////////////////////////
+
+		semaphore_handle = CreateSemaphoreA(
+			nullptr,
+			0,
+			MAX_LOCKS,
+			name.c_str()
+		);
+
+		if (semaphore_handle == nullptr)
+		{
+			throw std::runtime_error("could not create rwlock.");
+		}
+
+		if (GetLastError() == ERROR_ALREADY_EXISTS)
+		{
+			for (int i = 0; i < MAX_LOCKS; i++)
+			{
+				auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
+
+				if (wait_result != WAIT_OBJECT_0)
+					throw std::runtime_error("failed to wait for semaphore object");
+			}
+		}
+	}
+
+	~IPC_WriteLock() noexcept
+	{
+		if (semaphore_handle)
+		{
+			ReleaseSemaphore(semaphore_handle, MAX_LOCKS, nullptr);
+			CloseHandle(semaphore_handle);
+		}
+
+		if (mutex_handle)
+		{
+			ReleaseMutex(mutex_handle);
+			CloseHandle(mutex_handle);
+		}
+	}
+
+	IPC_WriteLock(const IPC_WriteLock&) = delete;
+	IPC_WriteLock& operator=(IPC_WriteLock const&) = delete;
+
+	IPC_WriteLock(IPC_WriteLock&& ipc_write_lock) noexcept :
+		semaphore_handle(std::exchange(ipc_write_lock.semaphore_handle, nullptr)),
+		mutex_handle(std::exchange(ipc_write_lock.mutex_handle, nullptr)) { }
+
+	IPC_WriteLock& operator=(IPC_WriteLock&& ipc_write_lock)
+	{
+		semaphore_handle = std::exchange(ipc_write_lock.semaphore_handle, nullptr);
+		mutex_handle = std::exchange(ipc_write_lock.mutex_handle, nullptr);
+
+		return *this;
+	}
+private:
+	HANDLE semaphore_handle = nullptr;
+	HANDLE mutex_handle = nullptr;
+};
+
+struct IPC_Object
+{
+
+
+	//size_t size = 0;
+	//uint8_t buffer[BLOCK_SIZE];
+};
+
+/*class IPC_Value {
+public:
+	IPC_Value(const std::string & key, const std::string& read_lock_name, IPC_Object* ipc_object)
+		: m_ipc_object(ipc_object), m_read_lock(read_lock_name) {}
+
+	IPC_Value(const IPC_Value&) = delete;
+	IPC_Value& operator=(IPC_Value const&) = delete;
+
+	IPC_Value(IPC_Value&& ipc_value) noexcept :
+		m_ipc_object(std::exchange(ipc_value.m_ipc_object, nullptr)),
+		m_read_lock(std::move(ipc_value.m_read_lock))
+	{ }
+
+	IPC_Value& operator=(IPC_Value&& ipc_value)
+	{
+		m_ipc_object = std::exchange(ipc_value.m_ipc_object, nullptr);
+		m_read_lock = std::move(ipc_value.m_read_lock);
+
+		return *this;
+	}
+
+	inline const uint8_t* buffer() {
+		return m_ipc_object ? m_ipc_object->buffer : nullptr;
+	}
+
+	inline size_t size() {
+		return m_ipc_object ? m_ipc_object->size : 0;
+	}
+private:
+	IPC_Object* m_ipc_object;
+	IPC_ReadLock m_read_lock;
+};*/
