12c11a6245ab5bb8913cefb0ea8b4df4ceb99ad1
vldr <vlader101@gmail.com>
2020-06-28 01:29:48 -0500

    Added transactional methods.

--- IPCKV/ipc_kv.cpp
+++ IPCKV/ipc_kv.cpp
@@ -3,117 +3,237 @@
 IPC_KV::IPC_KV(const std::string& name)
 {
 	m_name = name;
+	m_controller = new IPC_KV_Controller();
 
 	initialize_info(m_name);
-	initialize_data(m_name);
+	initialize_data(m_name); 
 }
 
 IPC_KV::~IPC_KV()
 {
-	if (m_data)
-		UnmapViewOfFile(m_data);
-
-	if (m_info)
-		UnmapViewOfFile(m_info);
-
-	if (m_data_handle)
-		CloseHandle(m_data_handle);
-
-	if (m_info_handle)
-		CloseHandle(m_info_handle);
+	if (m_controller)
+		delete m_controller;
 }
 
-bool IPC_KV::set(const std::string& key, unsigned char* data, size_t size)
+void IPC_KV::initialize_info(const std::string& name)
 {
+	auto handle_path = "ipckv_i_" + name;
+
+	if (handle_path.length() > MAX_PATH)
+	{
+		throw std::runtime_error("key is too long.");
+	}
+	
+	//////////////////////////////////////////////////
+
+	auto info_handle = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		sizeof(IPC_KV_Info),
+		handle_path.c_str()
+	);
+
+	if (info_handle == NULL)
+	{
+		throw std::runtime_error("could not create file mapping object.");
+	}
+
+	//////////////////////////////////////////////////
+
+	bool does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
+
+	auto buffer = MapViewOfFile(
+		info_handle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		sizeof(IPC_KV_Info)
+	);
+
+	if (buffer == NULL)
+	{
+		CloseHandle(info_handle);
+
+		throw std::runtime_error("could not map view of file.");
+	}
+
+	//////////////////////////////////////////////////
+
+	m_controller->m_info = (IPC_KV_Info*)buffer;
+	m_controller->m_info_handle = info_handle;
+
+	//////////////////////////////////////////////////
+
+	if (!does_already_exist)
+	{
+		printf("Initializing info %s...\n", handle_path.c_str());
+
+		m_controller->m_info->m_buffer_state = false;
+
+		m_controller->m_info->m_size[0] = 0;
+		m_controller->m_info->m_size[1] = 0;
+
+		m_controller->m_info->m_resize_count[0] = 0;
+		m_controller->m_info->m_resize_count[1] = 0;
+
+		m_controller->m_info->m_capacity[0] = INITIAL_CAPACITY;
+		m_controller->m_info->m_capacity[1] = INITIAL_CAPACITY;
+	}
+
+	m_resize_count = m_controller->getResizeCount();
+}
+
+void IPC_KV::initialize_data(const std::string& name)
+{
+	auto allocation_size = sizeof(IPC_KV_Data) * m_controller->getCapacity();
+	auto resize_count = m_controller->getResizeCount();
+
+	///////////////////////////////////////////
+
+	auto handle_path = "ipckv_" + std::to_string(resize_count) + "_" + name;
+
+	if (handle_path.length() > MAX_PATH)
+	{
+		throw std::runtime_error("key is too long.");
+	}
+
+	///////////////////////////////////////////
+
+	auto data_handle = CreateFileMapping(
+		INVALID_HANDLE_VALUE,
+		NULL,
+		PAGE_READWRITE,
+		0,
+		allocation_size,
+		handle_path.c_str()
+	);
+
+	if (data_handle == NULL)
+	{
+		throw std::runtime_error("could not create file mapping object.");
+	}
+
+	///////////////////////////////////////////
+
+	auto does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
+
+	auto buffer = MapViewOfFile(
+		data_handle,
+		FILE_MAP_ALL_ACCESS,
+		0,
+		0,
+		allocation_size
+	);
+
+	if (buffer == NULL)
+	{
+		CloseHandle(data_handle);
+
+		throw std::runtime_error("could not map view of file.");
+	}
+
+	///////////////////////////////////////////
+
+	m_controller->m_data = (IPC_KV_Data*)buffer;
+	m_controller->m_data_handle = data_handle;
+
+	///////////////////////////////////////////
+
+	if (!does_already_exist)
+	{
+		printf("Initializing data %s...\n", handle_path.c_str());
+
+		std::memset(m_controller->m_data, 0, allocation_size);
+	}
+}
+
+void IPC_KV::set(const std::string& key, unsigned char* data, size_t size)
+{
+	auto lock = get_lock(WRITE_LOCK);
+
 	if (size >= DATA_SIZE)
-		return false;
+		throw std::runtime_error("data size is too big");
 
-	IPC_WriteLock write_lock(m_lock_path);
+	if (key.length() >= KEY_SIZE - 1)
+		throw std::runtime_error("key size is too big");
 
+	set_internal(key, data, size);
+}
+
+void IPC_KV::set_internal(const std::string& key, unsigned char* data, size_t size)
+{
 	if (LOAD_FACTOR >= MAX_LOAD_FACTOR)
 		resize();
-	else
-		check_capacity();
-
+	 
 	uint32_t probeIndex = 0;
 	uint32_t bucketsProbed = 0;
 
+	uint32_t capacity = m_controller->getCapacity();
+
 	uint32_t hashCode = hash(key.c_str(), key.length());
-	uint32_t bucket = hashCode % m_info->capacity;
+	uint32_t bucket = hashCode % capacity; 
 
-	while (bucketsProbed < m_info->capacity)
+	while (bucketsProbed < m_controller->getCapacity())
 	{
-		if (m_data[bucket].state != IPC_KV_Data_State::Occupied || m_data[bucket].key == key)
+		if (m_controller->getDataState(bucket) != IPC_KV_Data_State::Occupied || m_controller->getDataKey(bucket) == key)
 		{ 
-			if (m_data[bucket].state != IPC_KV_Data_State::Occupied)
-				m_info->size++;
+			if (m_controller->getDataState(bucket) != IPC_KV_Data_State::Occupied)
+			{
+				m_controller->startInfoTransaction();
+				m_controller->setSize(m_controller->getSize() + 1);
+				m_controller->commitInfo();
+			}
 
-			m_data[bucket].size = size;
-			m_data[bucket].state = IPC_KV_Data_State::Occupied;
-
-			memcpy(m_data[bucket].data, data, size);
-			strncpy_s(m_data[bucket].key, key.c_str(), key.length());
-
-			return true;
+			m_controller->startDataTransaction(bucket);
+			m_controller->setDataKey(bucket, key.c_str(), key.length());
+			m_controller->setData(bucket, data, size);
+			m_controller->setDataState(bucket, IPC_KV_Data_State::Occupied);
+			m_controller->commitData(bucket);
+			
+			return;
 		}
 
 		printf("Collision! %s -> %d\n", key.c_str(), bucket);
 
 		probeIndex++;
 
-		bucket = (hashCode + C1_CONSTANT * probeIndex + C2_CONSTANT * probeIndex * probeIndex) % m_info->capacity;
+		bucket = (hashCode + C1_CONSTANT * probeIndex + C2_CONSTANT * probeIndex * probeIndex) % capacity;
 		bucketsProbed++;
 	}
 
-	return false;
+	throw std::runtime_error("unable to insert item due to unexpected error");
 }
 
 void IPC_KV::print()
 {
-	IPC_ReadLock read_lock(m_lock_path);
+	auto lock = get_lock(READ_LOCK);
 
-	check_capacity();
+	////////////////////////////////////////////////////
 
-	for (size_t i = 0; i < m_info->capacity; i++)
+	auto capacity = m_controller->getCapacity();
+
+	for (size_t i = 0; i < capacity; i++)
 	{
-		if (m_data[i].state == IPC_KV_Data_State::Occupied)
-			printf("[%d] %s 0x%X\n", i, m_data[i].key, m_data[i].size);
+		if (m_controller->getDataState(i) == IPC_KV_Data_State::Occupied)
+			printf("[%d] %s 0x%X\n", i, m_controller->getDataKey(i), m_controller->getDataSize(i));
 	}
 
-	printf("Capacity %d, Size %d, Resizes %d, Load Factor %f\n", m_info->capacity, m_info->size, m_info->resize_count, LOAD_FACTOR);
-}
- 
-size_t IPC_KV::size()
-{
-	return m_info->size;
-}
-
-size_t IPC_KV::find_nearest_prime(size_t input)
-{
-	while (!is_prime(input))
-	{
-		input++;
-	}
-
-	return input;
+	printf("Capacity %d, Size %d, Resizes %d, Load Factor %f\n", 
+		m_controller->getCapacity(), 
+		m_controller->getSize(), 
+		m_controller->getResizeCount(), 
+		LOAD_FACTOR
+	);
 }
 
-bool IPC_KV::is_prime(size_t input) 
-{
-	for (int i = 2; i <= (int)sqrt(input); i++)
-	{
-		if (input % i == 0)
-		{
-			return false;
-		}
-	}
 
-	return true;
-}
-
-void IPC_KV::check_capacity()
+IPC_Lock IPC_KV::get_lock(bool is_writing)
 {
-	if (m_cached_resize_count != m_info->resize_count)
+	IPC_Lock lock(is_writing, m_name);
+
+	/*if (m_cached_resize_count != m_info->resize_count)
 	{
 		printf("Capacity has changed, fetching new memory\n");
 
@@ -126,16 +246,16 @@ void IPC_KV::check_capacity()
 		initialize_data(m_name);
 
 		m_cached_resize_count = m_info->resize_count;
-		m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
-	}
-}
+	}*/
 
+	return lock;
+}
 
 void IPC_KV::resize()
 {
 	printf("Resizing memory.\n");
 
-	HANDLE previous_data_handle = m_data_handle;
+	/*HANDLE previous_data_handle = m_data_handle;
 	IPC_KV_Data* previous_data = m_data;
 	size_t previous_capacity = m_info->capacity;
 
@@ -146,13 +266,12 @@ void IPC_KV::resize()
 	initialize_data(m_name);
 
 	m_cached_resize_count = m_info->resize_count;
-	m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
 	
 	for (size_t i = 0; i < previous_capacity; i++)
 	{
 		if (previous_data[i].state == IPC_KV_Data_State::Occupied)
 		{
-			set(previous_data[i].key, previous_data[i].data, previous_data[i].size);
+			set_internal(previous_data[i].key, previous_data[i].data, previous_data[i].size);
 		}
 	}
 
@@ -160,7 +279,37 @@ void IPC_KV::resize()
 		UnmapViewOfFile(previous_data);
 
 	if (previous_data_handle)
-		CloseHandle(previous_data_handle);	
+		CloseHandle(previous_data_handle);	*/
+}
+
+size_t IPC_KV::size()
+{
+	auto lock = get_lock(READ_LOCK);
+
+	return m_controller->getSize();
+}
+
+size_t IPC_KV::find_nearest_prime(size_t input)
+{
+	while (!is_prime(input))
+	{
+		input++;
+	}
+
+	return input;
+}
+
+bool IPC_KV::is_prime(size_t input)
+{
+	for (int i = 2; i <= (int)sqrt(input); i++)
+	{
+		if (input % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
 }
 
 uint32_t IPC_KV::hash(const char * key, int count)
@@ -180,113 +329,9 @@ uint32_t IPC_KV::hash(const char * key, int count)
 	return h ^ (h >> 16);
 }
 
-void IPC_KV::initialize_data(const std::string& name)
-{
-	auto handle_path = "ipckv_" + std::to_string(m_info->resize_count) + "_" + name;
-
-	if (handle_path.length() > MAX_PATH)
-	{
-		throw std::runtime_error("key is too long.");
-	}
-
-	m_data_handle = CreateFileMapping(
-		INVALID_HANDLE_VALUE,
-		NULL,
-		PAGE_READWRITE,
-		0,
-		sizeof(IPC_KV_Data) * m_info->capacity,
-		handle_path.c_str()
-	);
-
-	if (m_data_handle == NULL)
-	{
-		throw std::runtime_error("could not create file mapping object.");
-	}
-
-	auto does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
-
-	auto buffer = MapViewOfFile(
-		m_data_handle,
-		FILE_MAP_ALL_ACCESS,
-		0,
-		0,
-		sizeof(IPC_KV_Data) * m_info->capacity
-	);
-
-	if (buffer == NULL)
-	{
-		CloseHandle(m_data_handle);
-
-		throw std::runtime_error("could not map view of file.");
-	}
-
-	m_data = (IPC_KV_Data*)buffer;
-
-	if (!does_already_exist)
-	{
-		printf("Initializing data %s...\n", handle_path.c_str());
-		std::memset(m_data, 0, sizeof(IPC_KV_Data) * m_info->capacity);
-	}
-}
-
-void IPC_KV::initialize_info(const std::string& name)
-{
-	auto handle_path = "ipckv_i_" + name;
-
-	if (handle_path.length() > MAX_PATH)
-	{
-		throw std::runtime_error("key is too long.");
-	}
-
-	m_info_handle = CreateFileMapping(
-		INVALID_HANDLE_VALUE,
-		NULL,
-		PAGE_READWRITE,
-		0,
-		sizeof(IPC_KV_Info),
-		handle_path.c_str()
-	);
-
-	if (m_info_handle == NULL)
-	{
-		throw std::runtime_error("could not create file mapping object.");
-	}
-
-	bool does_already_exist = GetLastError() == ERROR_ALREADY_EXISTS;
-
-	auto buffer = MapViewOfFile(
-		m_info_handle,
-		FILE_MAP_ALL_ACCESS,
-		0,
-		0,
-		sizeof(IPC_KV_Info)
-	);
-
-	if (buffer == NULL)
-	{
-		CloseHandle(m_info_handle);
-
-		throw std::runtime_error("could not map view of file.");
-	}
-
-	m_info = (IPC_KV_Info*)buffer;
-
-	if (!does_already_exist)
-	{
-		printf("Initializing info %s...\n", handle_path.c_str());
-
-		m_info->size = 0;
-		m_info->resize_count = 0;
-		m_info->capacity = INITIAL_CAPACITY;
-	}
-
-	m_lock_path = std::to_string(m_info->resize_count) + "_" + m_name;
-	m_cached_resize_count = m_info->resize_count;
-}
-
 int main()
 {
-	unsigned char lol[] = { 0x68, 0x69 };
+	unsigned char dummy_data[] = { 0x68, 0x69 };
 	auto kv = IPC_KV("test");
 
 	while (1)
@@ -298,7 +343,7 @@ int main()
 		if (!key.empty())
 		{
 			printf("Inserting %s\n", key.c_str());
-			kv.set(key, lol, sizeof(lol));
+			kv.set(key, dummy_data, sizeof(dummy_data));
 		}
 
 		kv.print();
--- IPCKV/ipc_kv.h
+++ IPCKV/ipc_kv.h
@@ -11,176 +11,389 @@
 #define DATA_SIZE 2048
 #define KEY_SIZE 260
 
-#define LOAD_FACTOR (float)m_info->size / (float)m_info->capacity
+#define LOAD_FACTOR (float)m_controller->getSize() / (float)m_controller->getCapacity()
 
 #define C1_CONSTANT 3
 #define C2_CONSTANT 5
 
-class IPC_KV 
+#define READ_LOCK false
+#define WRITE_LOCK true
+
+class IPC_Lock;
+
+enum IPC_KV_Data_State
+{
+	Empty = 0,
+	Deleted = 1,
+	Occupied = 2,
+};
+
+struct IPC_KV_Data
+{
+	IPC_KV_Data_State m_state[2];
+
+	char m_key[2][KEY_SIZE];
+	unsigned char m_value[2][DATA_SIZE];
+
+	size_t m_size[2];
+
+	bool m_buffer_state;
+};
+
+struct IPC_KV_Info
+{
+	bool m_buffer_state;
+	size_t m_capacity[2];
+	size_t m_size[2];
+	size_t m_resize_count[2];
+};
+
+class IPC_KV_Controller
 {
 public:
-	IPC_KV(const std::string& name);
-	~IPC_KV();
+	IPC_KV_Controller() {}
 
-	bool set(const std::string& key, unsigned char* data, size_t size);
-	void print();
-	size_t size();
-private:
-	enum IPC_KV_Data_State
+	~IPC_KV_Controller()
 	{
-		Empty = 0,
-		Deleted = 1,
-		Occupied = 2,
-	};
+		if (m_data)
+			UnmapViewOfFile(m_data);
 
-	struct IPC_KV_Info
+		if (m_info)
+			UnmapViewOfFile(m_info);
+
+		if (m_data_handle)
+			CloseHandle(m_data_handle);
+
+		if (m_info_handle)
+			CloseHandle(m_info_handle);
+	}
+
+	// Disallow copying and moving.
+	IPC_KV_Controller(const IPC_KV_Controller&) = delete;
+	IPC_KV_Controller(IPC_KV_Controller&&) = delete;
+	IPC_KV_Controller& operator=(const IPC_KV_Controller&) = delete;
+	IPC_KV_Controller& operator=(IPC_KV_Controller&&) = delete;
+
+	/**
+	* m_info Setters
+	*/
+
+	void startInfoTransaction()
 	{
-		size_t capacity;
-		size_t size;
+		setResizeCount(getResizeCount());
+		setCapacity(getCapacity());
+		setSize(getSize());
 
-		size_t resize_count;
-	};
+		has_started_info_transaction = true;
+	}
 
-	struct IPC_KV_Data
+	void setResizeCount(size_t resize_count)
 	{
-		IPC_KV_Data_State state;
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
 
-		char key[KEY_SIZE];
+		m_info->m_resize_count[!m_info->m_buffer_state] = resize_count;
+	}
 
-		unsigned char data[DATA_SIZE];
-		size_t size;
-	};
+	void setCapacity(size_t capacity)
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
 
-	size_t find_nearest_prime(size_t input);
-	bool is_prime(size_t input);
+		m_info->m_capacity[!m_info->m_buffer_state] = capacity;
+	}
 
-	void resize();
-	void check_capacity();
+	void setSize(size_t size)
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
 
-	uint32_t hash(const char* key, int count);
+		m_info->m_size[!m_info->m_buffer_state] = size;
+	}
 
-	void initialize_info(const std::string& name);
-	void initialize_data(const std::string& name);
+	/** 
+	* m_Data Setters
+	*/
+
+	void startDataTransaction(size_t index)
+	{
+		setDataSize(index, getDataSize(index));
+		setDataState(index, getDataState(index));
+		setData(index, getData(index), getDataSize(index));
+		setDataKey(index, getDataKey(index), strlen(getDataKey(index)));
+
+		has_started_data_transaction = true;
+	}
+
+	void setDataSize(size_t index, size_t size)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = !m_data[index].m_buffer_state;
+
+		m_data[index].m_size[buffer_state] = size;
+	}
+
+	void setDataState(size_t index, IPC_KV_Data_State state)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = !m_data[index].m_buffer_state;
+
+		m_data[index].m_state[buffer_state] = state;
+	}
+
+	void setData(size_t index, unsigned char* data, size_t size)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = !m_data[index].m_buffer_state;
+
+		memcpy_s(m_data[index].m_value[buffer_state], DATA_SIZE, data, size);
+		m_data[index].m_size[buffer_state] = size;
+	}
+
+	void setDataKey(size_t index, const char* key, size_t size)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = !m_data[index].m_buffer_state;
+
+		strncpy_s(m_data[index].m_key[buffer_state], key, size);
+	}
+
+	/**
+	* m_info Getters
+	*/
+
+	size_t getCapacity()
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
+
+		return m_info->m_capacity[m_info->m_buffer_state];
+	}
+
+	size_t getSize()
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
+
+		return m_info->m_size[m_info->m_buffer_state];
+	}
+
+	size_t getResizeCount()
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
+
+		return m_info->m_resize_count[m_info->m_buffer_state];
+	}
+
+	/**
+	* m_data Getters
+	*/
+
+	unsigned char* getData(size_t index)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = m_data[index].m_buffer_state;
+
+		return m_data[index].m_value[buffer_state];
+	}
+
+	size_t getDataSize(size_t index)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = m_data[index].m_buffer_state;
+
+		return m_data[index].m_size[buffer_state];
+	}
+
+	IPC_KV_Data_State getDataState(size_t index)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = m_data[index].m_buffer_state;
+
+		return m_data[index].m_state[buffer_state];
+	}
+
+	char* getDataKey(size_t index)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		bool buffer_state = m_data[index].m_buffer_state;
+
+		return m_data[index].m_key[buffer_state];
+	}
+
+	/**
+	* Commit Info
+	*/
+	void commitInfo() 
+	{
+		if (!m_info)
+			throw std::runtime_error("class is in an invalid state.");
+
+		if (!has_started_info_transaction)
+			throw std::runtime_error("a info transaction has not been started.");
+
+		m_info->m_buffer_state = !m_info->m_buffer_state;
+	}
+	/**
+	* Commit Data
+	*/
+	void commitData(size_t index)
+	{
+		if (!m_data)
+			throw std::runtime_error("class is in an invalid state.");
+
+		if (!has_started_data_transaction)
+			throw std::runtime_error("a data transaction has not been started.");
+
+		m_data[index].m_buffer_state = !m_data[index].m_buffer_state;
+	}
+
+	bool has_started_info_transaction = false;
+	bool has_started_data_transaction = false;
 
 	IPC_KV_Info* m_info = nullptr;
 	IPC_KV_Data* m_data = nullptr;
 
 	HANDLE m_info_handle = nullptr;
-	HANDLE m_data_handle = nullptr;	
-
-	std::string m_name;
-	std::string m_lock_path;
-
-	size_t m_cached_resize_count;
+	HANDLE m_data_handle = nullptr;
 };
 
-class IPC_ReadLock {
+class IPC_KV 
+{
 public:
-	IPC_ReadLock(const std::string& name)
-	{
-		if (name.length() > MAX_PATH)
-		{
-			throw std::runtime_error("rwlock name too long.");
-		}
+	/**
+	* Constructors and destructors
+	*/
+	IPC_KV(const std::string& name);
+	~IPC_KV();
 
-		semaphore_handle = CreateSemaphoreA(
-			nullptr,
-			MAX_LOCKS,
-			MAX_LOCKS,
-			name.c_str()
-		);
-
-		if (semaphore_handle == nullptr)
-		{
-			throw std::runtime_error("could not create rwlock.");
-		}
-
-		auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
-
-		if (wait_result != WAIT_OBJECT_0)
-			throw std::runtime_error("failed to wait for single object");
-	}
-
-	~IPC_ReadLock() noexcept
-	{
-		if (semaphore_handle)
-		{
-			ReleaseSemaphore(semaphore_handle, 1, nullptr);
-			CloseHandle(semaphore_handle);
-		}
-	}
-
-	IPC_ReadLock(const IPC_ReadLock&) = delete;
-	IPC_ReadLock& operator=(IPC_ReadLock const&) = delete;
-
-	IPC_ReadLock(IPC_ReadLock&& ipc_read_lock) noexcept :
-		semaphore_handle(std::exchange(ipc_read_lock.semaphore_handle, nullptr)) { }
-
-	IPC_ReadLock& operator=(IPC_ReadLock&& ipc_read_lock)
-	{
-		semaphore_handle = std::exchange(ipc_read_lock.semaphore_handle, nullptr);
-
-		return *this;
-	}
-
+	/**
+	* Public Methods
+	*/
+	void set(const std::string& key, unsigned char* data, size_t size);
+	void print();
+	size_t size();
 private:
-	HANDLE semaphore_handle = nullptr;
+	/**
+	* Private Methods
+	*/
+	void initialize_info(const std::string& name);
+	void initialize_data(const std::string& name);
+	void resize();
+	void set_internal(const std::string& key, unsigned char* data, size_t size);
+	bool is_prime(size_t input);
+
+	size_t find_nearest_prime(size_t input);
+	uint32_t hash(const char* key, int count);
+	IPC_Lock get_lock(bool is_writing);
+
+	/**
+	* Private Members
+	*/
+	IPC_KV_Controller* m_controller = nullptr;
+	std::string m_name;
+	size_t m_resize_count;
 };
 
-class IPC_WriteLock {
+class IPC_Lock {
 public:
-	IPC_WriteLock(const std::string& name)
+	IPC_Lock(bool is_write_lock, const std::string& name)
 	{
-		if (name.length() > MAX_PATH)
+		if (is_write_lock)
 		{
-			throw std::runtime_error("rwlock name too long.");
-		}
-
-		auto mutex_name = name + "_mutex";
-
-		mutex_handle = CreateMutexA(
-			nullptr,
-			FALSE,
-			mutex_name.c_str()
-		);
-
-		if (mutex_handle == nullptr)
-		{
-			throw std::runtime_error("could not create mutex.");
-		}
-
-		auto wait_result = WaitForSingleObject(mutex_handle, INFINITE);
-
-		if (wait_result != WAIT_OBJECT_0)
-			throw std::runtime_error("failed to wait for semaphore object");
-
-		//////////////////////////////////////////////
-
-		semaphore_handle = CreateSemaphoreA(
-			nullptr,
-			0,
-			MAX_LOCKS,
-			name.c_str()
-		);
-
-		if (semaphore_handle == nullptr)
-		{
-			throw std::runtime_error("could not create rwlock.");
-		}
-
-		if (GetLastError() == ERROR_ALREADY_EXISTS)
-		{
-			for (int i = 0; i < MAX_LOCKS; i++)
+			if (name.length() > MAX_PATH)
 			{
-				auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
+				throw std::runtime_error("rwlock name too long.");
+			}
 
-				if (wait_result != WAIT_OBJECT_0)
-					throw std::runtime_error("failed to wait for semaphore object");
+			auto mutex_name = name + "_mutex";
+
+			mutex_handle = CreateMutexA(
+				nullptr,
+				FALSE,
+				mutex_name.c_str()
+			);
+
+			if (mutex_handle == nullptr)
+			{
+				throw std::runtime_error("could not create mutex.");
+			}
+
+			auto wait_result = WaitForSingleObject(mutex_handle, INFINITE);
+
+			if (wait_result != WAIT_OBJECT_0)
+				throw std::runtime_error("failed to wait for semaphore object");
+
+			//////////////////////////////////////////////
+
+			semaphore_handle = CreateSemaphoreA(
+				nullptr,
+				0,
+				MAX_LOCKS,
+				name.c_str()
+			);
+
+			if (semaphore_handle == nullptr)
+			{
+				throw std::runtime_error("could not create rwlock.");
+			}
+
+			if (GetLastError() == ERROR_ALREADY_EXISTS)
+			{
+				for (int i = 0; i < MAX_LOCKS; i++)
+				{
+					auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
+
+					if (wait_result != WAIT_OBJECT_0)
+						throw std::runtime_error("failed to wait for semaphore object");
+				}
 			}
 		}
+		else
+		{
+			if (name.length() > MAX_PATH)
+			{
+				throw std::runtime_error("rwlock name too long.");
+			}
+
+			semaphore_handle = CreateSemaphoreA(
+				nullptr,
+				MAX_LOCKS,
+				MAX_LOCKS,
+				name.c_str()
+			);
+
+			if (semaphore_handle == nullptr)
+			{
+				throw std::runtime_error("could not create rwlock.");
+			}
+
+			auto wait_result = WaitForSingleObject(semaphore_handle, INFINITE);
+
+			if (wait_result != WAIT_OBJECT_0)
+				throw std::runtime_error("failed to wait for single object");
+		}
+
 	}
 
-	~IPC_WriteLock() noexcept
+	~IPC_Lock() noexcept
 	{
 		if (semaphore_handle)
 		{
@@ -195,14 +408,14 @@ public:
 		}
 	}
 
-	IPC_WriteLock(const IPC_WriteLock&) = delete;
-	IPC_WriteLock& operator=(IPC_WriteLock const&) = delete;
+	IPC_Lock(const IPC_Lock&) = delete;
+	IPC_Lock& operator=(IPC_Lock const&) = delete;
 
-	IPC_WriteLock(IPC_WriteLock&& ipc_write_lock) noexcept :
+	IPC_Lock(IPC_Lock&& ipc_write_lock) noexcept :
 		semaphore_handle(std::exchange(ipc_write_lock.semaphore_handle, nullptr)),
 		mutex_handle(std::exchange(ipc_write_lock.mutex_handle, nullptr)) { }
 
-	IPC_WriteLock& operator=(IPC_WriteLock&& ipc_write_lock)
+	IPC_Lock& operator=(IPC_Lock&& ipc_write_lock)
 	{
 		semaphore_handle = std::exchange(ipc_write_lock.semaphore_handle, nullptr);
 		mutex_handle = std::exchange(ipc_write_lock.mutex_handle, nullptr);
